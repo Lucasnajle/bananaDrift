1462827fad0cd05e949db0c46ca4dba9a59d5dd0
lvassallo <39530082+lvassallo@users.noreply.github.com>
2024-11-23 11:53:26 -0300

    Handle and equilibrium system

--- Assets/Scripts/ApplyForceOnKeyPress.cs
+++ Assets/Scripts/ApplyForceOnKeyPress.cs
@@ -4,19 +4,20 @@ public class ApplyForceOnKeyPress : MonoBehaviour
 {
     public Rigidbody targetRigidbody;  // Assign the Rigidbody in the Inspector
     public float forceAmount = 10f;    // Amount of force to apply
+    public float upwardsCoef = 0.2f;
 
     void Update()
     {
         // Check for Left Arrow key press
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            ApplyForce(Vector3.left);  // Apply force to the left (local space)
+            ApplyForce(Vector3.left + upwardsCoef * Vector3.up);  // Apply force to the left (local space)
         }
 
         // Check for Right Arrow key press
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            ApplyForce(Vector3.right); // Apply force to the right (local space)
+            ApplyForce(Vector3.right + upwardsCoef * Vector3.up); // Apply force to the right (local space)
         }
     }
 
