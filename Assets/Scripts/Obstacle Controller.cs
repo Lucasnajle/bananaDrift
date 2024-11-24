042a0c85f9989388ca73279d10c86149482b354d
lvassallo <39530082+lvassallo@users.noreply.github.com>
2024-11-24 15:44:23 -0300

    mas meshes

--- Assets/Scripts/Obstacle Controller.cs
+++ Assets/Scripts/Obstacle Controller.cs
@@ -5,7 +5,7 @@ public class ObstacleController : MonoBehaviour
     public Vector3 direction;
     public float force;
     void OnTriggerEnter(Collider theCollider) {
-        if (theCollider.gameObject.name.Contains("Hips")) {
+        if (theCollider.gameObject.name.Contains("Root")) {
             GameController.Instance.playerList[GameController.Instance.currentPlayerIndex].GetComponent<ApplyForceOnKeyPress>().ApplyForce(direction + 0.2f * Vector3.up, force);
         }
     }
